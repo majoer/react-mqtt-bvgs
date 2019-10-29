7a427a47fce484cda55be577f38fa2628ef5ed40
Jørgensen, Mats <matsjoer@gmail.com>
2019-10-29 13:33:27 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Paho from 'paho-mqtt'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+var MQTT_HOST = "test.mosquitto.org"
+var MQTT_PORT = 8081
+var MQTT_TOPIC = "bvgs"
+
+
+var client = new Paho.Client(MQTT_HOST, MQTT_PORT, "clientId");
+
+
+client.connect({
+  useSSL: true,
+  onSuccess: function () {
+    console.log('connected to broker')
+    client.subscribe(MQTT_TOPIC)
+  }
+});
+
+
+class App extends Component{
+
+  state = {
+    message: ''
+  }
+
+  componentDidMount() {
+
+    client.onMessageArrived = (message) => {
+      console.log(message)
+      this.setState({ message: message.payloadString })
+    };
+  }
+
+  render() {
+    const { message } = this.state
+    return (
+      <div className="App" >
+        <header className="App-header">
+          {message}
       </header>
-    </div>
-  );
+      </div>
+    )
+  }
 }
 
 export default App;
